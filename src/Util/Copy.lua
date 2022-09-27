0a26f657c544bbf921142a33de2964b38ea27036
L3G5ND <malachi_vaillancourt@icloud.com>
2022-09-27 16:25:28 -0400

    Util.Copy now allows cycle tables

--- src/Util/Copy.lua
+++ src/Util/Copy.lua
@@ -1,9 +1,18 @@
-local function copy(tbl)
+local function copy(tbl, cache)
+	if not cache then
+		cache = {tbl = true}
+	end
+
 	local newTable = {}
 	local mt = getmetatable(tbl)
 	for i, v in pairs(tbl) do
 		if typeof(v) == "table" then
-			newTable[i] = copy(v)
+			if cache[v] then
+				newTable[i] = v
+			else
+				cache[v] = true
+				newTable[i] = copy(v, cache)
+			end
 		else
 			newTable[i] = v
 		end
